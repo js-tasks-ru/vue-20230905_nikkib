d1c3719321dc8cbe9636cff3882d6f8521309291
Veronica Buravkova <veronica@webmercs.com>
2023-09-09 16:18:28 +0400

    Add 01-basics\01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,21 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+    name: 'App',
+
+    data() {
+        return {
+            count: 0,
+        };
+    },
+
+    methods: {
+        increase() {
+          this.count++
+        }
+    },
+})
+
+const app = createApp(App);
+
+app.mount('#app');
