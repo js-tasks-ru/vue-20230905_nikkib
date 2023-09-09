c3c117791a3124312e8916804be30f31e1fe8ded
Veronica Buravkova <veronica@webmercs.com>
2023-09-09 23:26:28 +0400

    Add 01-basics\02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,29 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+    name: 'App',
+
+    data() {
+        return {
+            num1: 0,
+            num2: 0,
+            operator: 'sum',
+        };
+    },
+
+    computed: {
+        result() {
+          const allResults = {
+            sum: () => this.num1 + this.num2,
+            subtract: () => this.num1 - this.num2,
+            multiply: () => this.num1 * this.num2,
+            divide: () => this.num1 / this.num2
+          }
+          return allResults[this.operator]()
+        }
+      }
+})
+
+const app = createApp(App);
+
+app.mount('#app');
