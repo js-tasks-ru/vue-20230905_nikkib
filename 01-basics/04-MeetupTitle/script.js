b70c9baf2165892bbaec672e3cfbec530d4b0308
Veronica Buravkova <veronica@webmercs.com>
2023-09-12 14:52:16 +0400

    Add 01-basics\04-MeetupTitle

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,45 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
-      });
-    }
-  });
-}
+const App = defineComponent({
+  name: 'App',
 
-// Требуется создать Vue приложение
+  data() {
+    return {
+      fetchMeetupById: null,
+      meetupTitle: '',
+    };
+  },
+
+  watch: {
+    fetchMeetupById: 
+      function(selectedValue) {
+        this.fetchMeetupTitle(selectedValue);
+    },
+  },
+  methods: {
+    fetchMeetupTitle(meetupId) {
+      fetch(`${API_URL}/meetups/${meetupId}`)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return response.json().then((error) => {
+              throw error;
+            });
+          }
+        })
+        .then((data) => {
+          this.meetupTitle = data.title;
+        })
+        .catch((error) => {
+          console.error('Error while getting meetup data:', error);
+        });
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.mount('#app');
