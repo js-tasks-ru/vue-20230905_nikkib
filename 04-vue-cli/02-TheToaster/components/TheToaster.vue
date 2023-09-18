8c50f1cf76809ea4043544e13cf008f2b17f3258
Veronica Buravkova <veronica@webmercs.com>
2023-09-18 20:48:26 +0400

    Add 04-vue-cli/02-The-Toaster

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -1,24 +1,52 @@
 <template>
   <div class="toasts">
-    <div class="toast toast_success">
-      <UiIcon class="toast__icon" icon="check-circle" />
-      <span>Success Toast Example</span>
-    </div>
-
-    <div class="toast toast_error">
-      <UiIcon class="toast__icon" icon="alert-circle" />
-      <span>Error Toast Example</span>
-    </div>
+    <UiToast v-for="(toast, index) in toasts" 
+      :key="index" 
+      :toastType="toast.type" 
+      :message="toast.message"
+      @close="close(index)"/>
   </div>
 </template>
 
 <script>
-import UiIcon from './UiIcon.vue';
+import UiToast from './UiToast.vue';
 
 export default {
   name: 'TheToaster',
 
-  components: { UiIcon },
+  data() {
+    return {
+      toasts: []
+    }
+  },
+
+  components: { UiToast },
+
+  methods: {
+    success(message) {
+      this.toasts.push({type: "success", message});
+      const toastsCopy = [...this.toasts];
+      this.toasts = toastsCopy;
+    },
+
+    error(message) {
+      this.toasts.push({type: "error", message});
+      const toastsCopy = [...this.toasts];
+      this.toasts = toastsCopy;
+    },
+
+    close(index) {
+      this.toasts.splice(index, 1);
+    },
+  },
+
+  watch: {
+    toasts() {
+      setTimeout(() => {
+        this.toasts.shift();
+      }, 5000);
+    },
+  },
 };
 </script>
 
@@ -39,35 +67,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
