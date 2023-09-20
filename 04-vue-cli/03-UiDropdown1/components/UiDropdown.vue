ab286b4770615939ce63f0c7b330b40950ac2bee
Veronica Buravkova <veronica@webmercs.com>
2023-09-20 11:00:24 +0400

    Add 04-vue-cli/03-UiDropdown1

--- 04-vue-cli/03-UiDropdown1/components/UiDropdown.vue
+++ 04-vue-cli/03-UiDropdown1/components/UiDropdown.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="dropdown dropdown_opened">
-    <button type="button" class="dropdown__toggle dropdown__toggle_icon">
-      <UiIcon icon="tv" class="dropdown__icon" />
-      <span>Title</span>
+  <div :class="['dropdown', {'dropdown_opened': isOpened}]">
+    <button type="button" :class="['dropdown__toggle', {'dropdown__toggle_icon': iconDropDown}]" @click="isOpened = !isOpened">
+      <UiIcon v-if="selectedIcon" :icon="selectedIcon" class="dropdown__icon" />
+      <span>{{ selectedText }}</span>
     </button>
 
-    <div class="dropdown__menu" role="listbox">
-      <button class="dropdown__item dropdown__item_icon" role="option" type="button">
-        <UiIcon icon="tv" class="dropdown__icon" />
-        Option 1
-      </button>
-      <button class="dropdown__item dropdown__item_icon" role="option" type="button">
-        <UiIcon icon="tv" class="dropdown__icon" />
-        Option 2
+    <div v-show="isOpened" class="dropdown__menu" role="listbox">
+      <button v-for="option in options" :key="option.value" :class="['dropdown__item', {'dropdown__item_icon': iconDropDown}]" @click="getOption(option)" role="option" type="button">
+        <UiIcon v-if="option.icon" :icon="option.icon" class="dropdown__icon" />
+          {{ option.text }}
       </button>
     </div>
   </div>
@@ -24,7 +20,63 @@ import UiIcon from './UiIcon.vue';
 export default {
   name: 'UiDropdown',
 
+  data() {
+    return {
+      isOpened: false,
+    };
+  },
+
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+
+    modelValue: {
+      type: String,
+      default: null,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+
   components: { UiIcon },
+
+  emits: ['update:modelValue'],
+
+  computed: {
+    iconDropDown() {
+      let hasIcon = false;
+      this.options.forEach((option) => {
+        if (option.icon) {
+          hasIcon = true;
+        }
+      });
+      return hasIcon;
+    },
+
+    selectedOption() {
+      return this.options.find(item => item.value == this.modelValue);
+    },
+
+    selectedIcon() {
+      return this.modelValue ? this.selectedOption.icon : false;
+    },
+
+    selectedText() {
+      return this.modelValue ? this.selectedOption.text : this.title
+    },
+  },
+
+  methods: {
+    getOption(option) {
+      this.isOpened = false;
+      this.$emit('update:modelValue', option.value);
+    },
+  }
 };
 </script>
 
